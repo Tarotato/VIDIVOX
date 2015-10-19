3ff9794730c13f95de43e5f892cb71206d8b62f9
Tarotato <izhu678@aucklanduni.ac.nz>
2015-10-19 17:57:00 +1300

    CommentaryTextArea: edit comments

--- CommentaryTextArea.java
+++ CommentaryTextArea.java
@@ -6,24 +6,31 @@ import java.awt.event.FocusListener;
 
 import javax.swing.JTextArea;
 
+/**
+ * @author Isabel Zhuang
+ * Class sets the text area when the user first opens VIDIVOX
+ * This occurs only once when VIDIVOX is first run
+ */
+@SuppressWarnings("serial")
 public class CommentaryTextArea extends JTextArea implements FocusListener{
 	
 	boolean i = true;
 	
 	public CommentaryTextArea(){
 		this.addFocusListener(this);
-		// Placeholder for when VIDIVOX is first run
+		// Placeholder for text area when VIDIVOX is first run
 		this.setText("Enter your commentary here"+ System.getProperty("line.separator") +"(Max. 40 words)");
 		this.setForeground(Color.GRAY);		
 	}
 
+	/* (non-Javadoc)
+	 * @see java.awt.event.FocusListener#focusGained(java.awt.event.FocusEvent)
+	 */
 	@Override
 	public void focusGained(FocusEvent e) {
-		// Removes the placeholder for the user to enter their own text when selected
-		// This occurs only once when VIDIVOX is first run
 		if(i){
-			this.setText("");
-			this.setForeground(Color.BLACK);
+			this.setText(""); // Removes the placeholder for the user to enter their own text when selected		
+			this.setForeground(Color.BLACK); 
 			i = false;
 		}		
 	}
