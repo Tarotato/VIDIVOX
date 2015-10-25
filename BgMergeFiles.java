df7f2d164ac69241f229d827ec028f884ddcf3eb
Tarotato <izhu678@aucklanduni.ac.nz>
2015-10-26 10:01:26 +1300

    BgMergeFiles: Error Handling

--- BgMergeFiles.java
+++ BgMergeFiles.java
@@ -1,18 +1,25 @@
-package vidivox_beta;
-
+package background_tasks;
 import javax.swing.JFrame;
 import javax.swing.JOptionPane;
 import javax.swing.SwingWorker;
 
-import uk.co.caprica.vlcj.player.MediaPlayer;
+import commentary_manipulation.PleaseWaitDialog;
 
-public class BgMergeFiles extends SwingWorker<Void, Void> {
-	
+import uk.co.caprica.vlcj.player.MediaPlayer;
+import video_manipulation.ProgressDisplayPanel;
+import vidivox_beta.HelperFile;
+import vidivox_beta.MainFrame;
+
+/** 
+ * @author Isabel Zhuang
+ * Class merges a selected audio file (mp3) with the current video in a back ground task
+ */
+public class BgMergeFiles extends SwingWorker<Void, Void> {	
 	private String videoPath;
 	private String mp3Path;
 	private PleaseWaitDialog waitDialog;
 	private JFrame thisFrame;
-	private MediaPlayer video;
+	private MediaPlayer video;	
 	
 	public BgMergeFiles(String videoPath, String mp3Path, PleaseWaitDialog waitDialog, JFrame thisFrame, MediaPlayer video) {
 		this.videoPath = videoPath;
@@ -20,14 +27,13 @@ public class BgMergeFiles extends SwingWorker<Void, Void> {
 		this.waitDialog = waitDialog;
 		this.thisFrame = thisFrame;
 		this.video = video;
-	}
-	
+	}	
 	@Override
 	protected Void doInBackground() throws Exception {
 		
 		waitDialog.setModal(true);
 		
-		// Deletes any existing output.mp3 file
+		// Deletes any existing vidAudio.mp3 file
 		String cmd = "rm -r ./MP3Files/.vidAudio.mp3";
 		ProcessBuilder builder = new ProcessBuilder("/bin/bash", "-c", cmd);
 		Process process = builder.start();
@@ -39,20 +45,37 @@ public class BgMergeFiles extends SwingWorker<Void, Void> {
 		process = builder.start();
 		process.waitFor();
 		
-		// Deletes any existing output.mp3 file
-		cmd = "rm -r ./MP3Files/.output.mp3";
-		builder = new ProcessBuilder("/bin/bash", "-c", cmd);
-		process = builder.start();
-		process.waitFor();
-		
-		// Creates an mp3 file output.mp3 that combines the video audio and selected mp3 audio for merging 
-		cmd = "ffmpeg -i " + mp3Path + " -i ./MP3Files/.vidAudio.mp3 -filter_complex amix=inputs=2 ./MP3Files/.output.mp3"; 
-		builder = new ProcessBuilder("/bin/bash", "-c", cmd);
-		process = builder.start();
-		process.waitFor();
+		// Creates an mp3 file output.mp3 that combines the video audio and selected mp3 audio for merging		
+		if(ProgressDisplayPanel.silenceTime>0){
+			// Creates an audio file with a silence at the start if insert time specified
+			System.out.println(ProgressDisplayPanel.silenceTime+"addsilence");
+			int silenceLength = ProgressDisplayPanel.silenceTime;
+			//Creates a silent wav file
+			cmd = "ffmpeg -y -f lavfi -i aevalsrc=0:0:0:0:0:0:0::duration="+silenceLength+" ./MP3Files/.silence.wav";
+			builder = new ProcessBuilder("/bin/bash", "-c", cmd);
+			process = builder.start();
+			process.waitFor();
+			
+			// Concatenates the silent wav file with the mp3 file
+			cmd = "ffmpeg -y -i ./MP3Files/.silence.wav -i "+mp3Path+" -filter_complex '[0:0][1:0]concat=n=2:v=0:a=1[combined];[combined]volume=1[out]' -map '[out]' './MP3Files/.output1.mp3'";
+			builder = new ProcessBuilder("/bin/bash", "-c", cmd);
+			process = builder.start();				
+			process.waitFor();
+			
+			cmd = "ffmpeg -y -i './MP3Files/.output1.mp3' -i './MP3Files/.vidAudio.mp3' -filter_complex amix=inputs=2 './MP3Files/.output.mp3'"; 
+			builder = new ProcessBuilder("/bin/bash", "-c", cmd);
+			process = builder.start();
+			process.waitFor();
+		}else{
+			// Else create an mp3 that merges the mp3 file at the start
+			cmd = "ffmpeg -y -i " + mp3Path + " -i ./MP3Files/.vidAudio.mp3 -filter_complex amix=inputs=2 ./MP3Files/.output.mp3"; 
+			builder = new ProcessBuilder("/bin/bash", "-c", cmd);
+			process = builder.start();
+			process.waitFor();
+		}
 		
 		// Creates an output.avi file from merging combined audio (0:0) and existing video stream (1:0)
-		cmd = "ffmpeg -i ./MP3Files/.output.mp3 -i "+ videoPath + " -map 0:0 -map 1:0 -acodec copy -vcodec copy ./VideoFiles/"+MainFrame.videoName[0]+".avi";
+		cmd = "ffmpeg -i "+ videoPath + " -i ./MP3Files/.output.mp3 -map 0:0 -map 1:0 -acodec copy -vcodec copy ./VideoFiles/"+MainFrame.videoName[0]+".avi";
 		builder = new ProcessBuilder("/bin/bash", "-c", cmd);
 		process = builder.start();
 		process.waitFor(); 
@@ -62,15 +85,26 @@ public class BgMergeFiles extends SwingWorker<Void, Void> {
 	
 	@Override
 	protected void done(){
-		//get rid of progress bar
+		// Close window with progress bar
 		waitDialog.dispose();
-		int n = JOptionPane.showConfirmDialog(thisFrame, "Successfully merged "+ HelperFile.getBasename(mp3Path) +" with "+ HelperFile.getBasename(videoPath) +".\n Would you like to play it now?", null, JOptionPane.OK_CANCEL_OPTION);
 		
-		if(n == 0) { // Change the video to output.avi if user selects "OK"
-			video.playMedia("VideoFiles/"+MainFrame.videoName[0]+".avi");
-			HelperFile.setCurrentVideoPath(System.getProperty("user.dir") + "VideoFiles/"+MainFrame.videoName[0]+".avi");
-		}	
-		
+		//Check if file has been created
+		if (HelperFile.isGenerated(MainFrame.videoName[0])) {
+			int n = JOptionPane.showConfirmDialog(thisFrame,"Successfully merged " + HelperFile.getBasename(mp3Path) + " with "
+							+ HelperFile.getBasename(videoPath) + ".\n Would you like to play it now?", null, JOptionPane.OK_CANCEL_OPTION);
+			if (n == 0) { 
+				// Change the video to output.avi if user selects "OK"
+				video.playMedia("VideoFiles/" + MainFrame.videoName[0] + ".avi");
+				HelperFile.setCurrentVideoPath(System.getProperty("user.dir") + "VideoFiles/" + MainFrame.videoName[0] + ".avi");
+				// Set button to play if video is paused
+				if (!MainFrame.playClicked) {
+					MainFrame.setToPlay();
+				}
+			}
+		}else{
+			// Show error if file is not created
+			JOptionPane.showMessageDialog(thisFrame, "Video creation failed: Please try again", "Error" , JOptionPane.ERROR_MESSAGE);
+		}		
 	}
 }
 
